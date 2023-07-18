a60375bcbde07a522647965716d84145965d6f72
Ben Leadbetter - Mac2 <ben.leadbetter@native-instruments.com>
2023-07-18 22:28:45 +0200

    app: introduce ncurses and sdl

--- app/main.cpp
+++ app/main.cpp
@@ -1,13 +1,28 @@
 #include <game/Game.h>
 #include <game/GameState.h>
+#include <ncurses.h>
 #include <render/Context.h>
 
+#include <chrono>
 #include <iostream>
 
 int main() {
   using namespace space_invaders;
+
   game::Game game;
   render::Context renderer(game.state().dimensions);
-  std::cout << renderer.render(game) << std::endl;
+
+  initscr();
+  cbreak();
+  keypad(stdscr, TRUE);
+  nodelay(stdscr, TRUE);
+  noecho();
+
+  while (true) {
+    auto tick = std::chrono::system_clock::now().time_since_epoch();
+    clear();
+  }
+
+  endwin();
   return 0;
 }
