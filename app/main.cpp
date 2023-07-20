72fd660845a572c4d216bfaed20419c39b7e4576
Ben Leadbetter - Mac2 <ben.leadbetter@native-instruments.com>
2023-07-20 23:10:20 +0200

    app: render with ncurses,
    * bug: does not render entire frame - just the last line

--- app/main.cpp
+++ app/main.cpp
@@ -1,10 +1,17 @@
 #include <game/Game.h>
 #include <game/GameState.h>
+#include <locale.h>
 #include <ncurses.h>
 #include <render/Context.h>
 
 #include <chrono>
 #include <iostream>
+#include <ranges>
+#include <thread>
+
+constexpr std::chrono::microseconds frameLength() {
+  return std::chrono::microseconds(40000);
+}
 
 int main() {
   using namespace space_invaders;
@@ -12,15 +19,35 @@ int main() {
   game::Game game;
   render::Context renderer(game.state().dimensions);
 
+  setlocale(LC_ALL, "");
   initscr();
   cbreak();
   keypad(stdscr, TRUE);
   nodelay(stdscr, TRUE);
+  scrollok(stdscr, FALSE);
   noecho();
 
+  const auto tick = []() {
+    const auto time = std::chrono::system_clock::now().time_since_epoch();
+    return std::chrono::duration_cast<std::chrono::microseconds>(time);
+  };
+
   while (true) {
-    auto tick = std::chrono::system_clock::now().time_since_epoch();
+    auto start = tick();
+
+    if (int ch = getch(); ch == 'q') {
+      break;
+    }
+
+    game.tick();
+
     clear();
+    printw("%s", renderer.render(game).c_str());
+    refresh();
+
+    if (const auto end = tick(); end - start < frameLength()) {
+      std::this_thread::sleep_for(frameLength() - (end - start));
+    }
   }
 
   endwin();
